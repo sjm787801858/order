4f87d042e9b424c36c65459d353194c327c99c1c
Administrator <Administrator@stu1003>
2018-06-04 18:50:14 +0800

    创建一个users实体类

---
+++ src/com/etc/Users.java
@@ -0,0 +1,25 @@
+package com.etc;
+
+public class Users {
+
+	
+	private int userId;
+	private String userName;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	@Override
+	public String toString() {
+		return "Users [userId=" + userId + ", userName=" + userName + "]";
+	}
+	
+}
